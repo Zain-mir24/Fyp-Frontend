c72b8498ab8ba89a6a324c76c1f751187a66ab1c
Zain-mir24 <zainnaeemk10@gmail.com>
2021-07-31 09:04:58 -0700

    Admin

--- src/components/Signin.js
+++ src/components/Signin.js
@@ -28,7 +28,7 @@ export default function SignIn() {
             <LockOutlinedIcon />
           </Avatar>
           <Typography component="h1" variant="h5">
-            Sign in
+          Global Reach  Sign in
           </Typography>
           <form className={classes.form} noValidate>
             <TextField
