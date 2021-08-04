7a6ad9f3aaae9897ceb67ce4fb173545cf953874
Zain-mir24 <zainnaeemk10@gmail.com>
2021-08-04 06:03:19 -0700

    desigining navbar for frontend

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
 
   useEffect(() => {
     
-    fetch("http://localhost:9000/home")
+    fetch("http://localhost:9000/admin")
       .then((res) => res.text())
       .then((res) => setApi(res));
   });
