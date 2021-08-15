72efb85a5cdceeeb6350d5ca00ba9e0a9bef8deb
Zain-mir24 <zainnaeemk10@gmail.com>
2021-08-15 02:07:18 -0700

    frontend

--- src/components/AdminPanel/Sidebar.js
+++ src/components/AdminPanel/Sidebar.js
@@ -1,36 +1,46 @@
-
 import React, { useState, useEffect } from "react";
-import axios from 'axios'
-const style={
-  float:"left"
-}
+import axios from "axios";
+import { DataGrid } from "@material-ui/data-grid";
+const style = {
+  float: "left",
+};
 export default function Sidebar() {
   const [getApi, setApi] = useState([]);
-  const getData=async()=>{
-    const response =await axios.get("http://localhost:9000/admin/users")
-    const data= await response.data
-    setApi(data)
-    console.log(data)
-  }
-  useEffect(async()=>{
-    getData()
-  },[]) 
-      
+  const getData = async () => {
+    const response = await axios.get("http://localhost:9000/admin/users");
+    const data = await response.data;
+    setApi(data);
+  };
+  useEffect(async () => {
+    getData();
+  }, []);
 
-  return <div style={style}>
-     {getApi.map((user) => {
-       const {_id,name,email}=user
-       console.log(name)
-          return (
-            <tr>
-
-              <td>{_id}</td>
-              <td>{name}</td>
-              <td>{email}</td>
-            </tr>
-          );
-        })}
- 
-        
-  </div>
+  return (
+    <div style={{ height: 250, width: "100%" }}>
+      {getApi.map((user) => {
+        const { _id, name, email } = user;
+        const rows = [
+          {
+            id: _id,
+            username: name,
+            Email: email,
+          },
+        ];
+        console.log(name);
+        return (
+          <DataGrid
+            columns={[
+              { field: "id", width: 200 },
+              {
+                field: "username",
+                width: 200,
+              },
+              { field: "Email", width: 200 },
+            ]}
+            rows={rows}
+          />
+        );
+      })}
+    </div>
+  );
 }
