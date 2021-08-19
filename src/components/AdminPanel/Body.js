ac75b533a1c177fe3147d967420bfc92b75a7443
AHMED KHALID <fa18-bcs-007@isbstudent.comsats.edu.pk>
2021-08-19 16:06:05 +0500

    Admin Panel Switching

--- src/components/AdminPanel/Body.js
+++ src/components/AdminPanel/Body.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from "react";
 import RightSide from "./RightSide";
 import Sidebar from "./Sidebar";
+import Paper from "@material-ui/core/Paper";
+import { withThemeCreator } from "@material-ui/styles";
 
 function Body() {
   const [rightBar, setRightBar] = useState("Dashboard");
@@ -16,11 +18,37 @@ function Body() {
   return (
     <div className="row">
       <div className="col-lg-2">
-        <Sidebar />
+        <div style={{ backgroundColor: "blue" }}>
+          <Paper
+            align="left"
+            style={{
+              paddingTop: "20px",
+              fontFamily: "sans-serif",
+              backgroundColor: "#368B85",
+              color: "white",
+            }}
+          >
+            <p style={styles.text}>Dashboard</p>
+            <p style={styles.text} onClick={() => setRightBar("Analytics")}>
+              Analytics
+            </p>
+            <p style={styles.text}>Campaign</p>
+            <p style={styles.text}>Revenue</p>
+            <p style={styles.text}>Orphans</p>
+            <p style={styles.text}>Set Carousel</p>
+            <p style={styles.text}>Upcoming Events</p>
+          </Paper>
+        </div>
       </div>
       <div className="col-lg-10">{displayComponent()}</div>
     </div>
   );
 }
 
+const styles = {
+  text: {
+    paddingLeft: "10px",
+    borderBottom: "0.5px solid white",
+  },
+};
 export default Body;
--- src/components/AdminPanel/Sidebar.js
+++ src/components/AdminPanel/Sidebar.js
@@ -3,27 +3,7 @@ import Paper from "@material-ui/core/Paper";
 import { withThemeCreator } from "@material-ui/styles";
 
 export default function Sidebar() {
-  return (
-    <div style={{ backgroundColor: "blue" }}>
-      <Paper
-        align="left"
-        style={{
-          paddingTop: "20px",
-          fontFamily: "sans-serif",
-          backgroundColor: "#368B85",
-          color: "white",
-        }}
-      >
-        <p style={styles.text}>Dashboard</p>
-        <p style={styles.text}>Analytics</p>
-        <p style={styles.text}>Campaign</p>
-        <p style={styles.text}>Revenue</p>
-        <p style={styles.text}>Orphans</p>
-        <p style={styles.text}>Set Carousel</p>
-        <p style={styles.text}>Upcoming Events</p>
-      </Paper>
-    </div>
-  );
+  return <div></div>;
 }
 
 const styles = {
