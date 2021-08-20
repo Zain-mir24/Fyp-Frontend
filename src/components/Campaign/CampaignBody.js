6651468dc4e48bfd23956bc6405d2a377ef94a00
AHMED KHALID <fa18-bcs-007@isbstudent.comsats.edu.pk>
2021-08-20 10:54:34 +0500

    Created Campaign Page and Card

---
+++ src/components/Campaign/CampaignBody.js
@@ -0,0 +1,15 @@
+import React, { useState, useEffect } from "react";
+import Paper from "@material-ui/core/Paper";
+import { withThemeCreator } from "@material-ui/styles";
+import Card from "./CampaignCard";
+
+function CampaignBody() {
+  return (
+    <div>
+      <Card />
+    </div>
+  );
+}
+
+const styles = {};
+export default CampaignBody;
