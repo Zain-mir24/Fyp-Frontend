0cbe3e9d5c53a09380e94c2e6a86df7f3077b83a
AHMED KHALID <fa18-bcs-007@isbstudent.comsats.edu.pk>
2021-08-21 13:45:47 +0500

    Created Campaign Page Frontend

--- src/components/Campaign/CampaignCard.js
+++ src/components/Campaign/CampaignCard.js
@@ -5,21 +5,28 @@ import { withThemeCreator } from "@material-ui/styles";
 function CampaignCard() {
   return (
     <div style={styles.container}>
-      <h1>Hello World</h1>
-      <h3>
-        Lorem Ipsum is simply dummy text of the printing and typesetting
-        industry.
+      <h1 style={styles.heading}>Rikshaw Campaign For Chaudhary Zain</h1>
+      <h3 style={styles.heading}>
+        Chaudhary Zain Naeem Mir Khawaja is very in need of Rikshaw as he cannot
+        find suitable job in field of Computer Science. Kindly donate some money
+        to help my mazloom friend.
       </h3>
+      <button className="btn btn-danger">DONATE NOW</button>
     </div>
   );
 }
 
 const styles = {
   container: {
-    backgroundImage: "url('./logo192.png')",
+    backgroundImage: "url('./Hepkursus-header.jpg')",
     backgroundRepeat: "no-repeat",
-    height: "700px",
-    textAlign: "left",
+    height: "300px",
+    paddingTop: "50px",
+    marginBottom: "30px",
+  },
+  heading: {
+    color: "white",
+    fontFamily: "Roboto-Mono",
   },
 };
 export default CampaignCard;
