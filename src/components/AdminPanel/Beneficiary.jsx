92be2a93f1f1ca729b3d53e25de99b36a72e4033
Zain-mir24 <zainnaeemk10@gmail.com>
2021-12-11 17:01:05 +0500

    frontend of users

--- src/components/AdminPanel/Beneficiary.jsx
+++ src/components/AdminPanel/Beneficiary.jsx
@@ -1,85 +1,83 @@
-import React, { useEffect, useState } from 'react'
-import { Table, Tag, Space,Button } from 'antd';
+import React, { useEffect, useState } from "react";
+import { Table, Tag, Space, Button } from "antd";
 const axios = require("axios");
-require('dotenv').config({ debug: process.env.DEBUG })
+require("dotenv").config({ debug: process.env.DEBUG });
 function Beneficiary() {
-  const [users,setUsers]=useState([])
-  const getUsers= async()=>{
-    try{
-      const res= await axios.get("http://localhost:9000/admin/users")
-        console.log(res.data,"Users")
-        setUsers(
-          res.data.map((i) => ({
-           userID:i._id,
-           name: i.name,
-           email: i.email,
-           userType: i.userType
-                   }))
-        );
-      }
-      catch(e){
-      console.log(e)
-      }
-  }
- 
-   
-const columns = [
+  useEffect(() => {
+    getUsers();
+  }, []);
+  const [users, setUsers] = useState([]);
+  const getUsers = async () => {
+    try {
+      const res = await axios.get("http://localhost:9000/admin/users");
+      console.log(res.data, "Users");
+      setUsers(
+        res.data.map((i) => ({
+          userID: i._id,
+          name: i.name,
+          email: i.email,
+          userType: i.userType,
+        }))
+      );
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  const columns = [
     {
-      title: 'Name',
-      dataIndex: 'name',
-      key: 'name',
-      render: text => <a>{text}</a>,
+      title: "Name",
+      dataIndex: "name",
+      key: "name",
+      render: (text) => <a>{text}</a>,
     },
     {
-      title: 'email',
-      dataIndex: 'email',
-      key: 'email',
+      title: "email",
+      dataIndex: "email",
+      key: "email",
     },
-   
+
     {
-      title: 'userType',
-      key: 'userType',
-      dataIndex: 'userType'
-    }, 
-    {
-      title: 'Userid',
-      key: 'userID',
-      dataIndex: 'userID'
+      title: "userType",
+      key: "userType",
+      dataIndex: "userType",
     },
-   
+    {
+      title: "Userid",
+      key: "userID",
+      dataIndex: "userID",
+    },
   ];
-  
+
   const data = [
     {
-      key: '1',
-      name: 'John Brown',
+      key: "1",
+      name: "John Brown",
       age: 32,
-      address: 'New York No. 1 Lake Park',
-      tags: ['nice', 'developer'],
+      address: "New York No. 1 Lake Park",
+      tags: ["nice", "developer"],
     },
     {
-      key: '2',
-      name: 'Jim Green',
+      key: "2",
+      name: "Jim Green",
       age: 42,
-      address: 'London No. 1 Lake Park',
-      tags: ['loser'],
+      address: "London No. 1 Lake Park",
+      tags: ["loser"],
     },
     {
-      key: '3',
-      name: 'Joe Black',
+      key: "3",
+      name: "Joe Black",
       age: 32,
-      address: 'Sidney No. 1 Lake Park',
-      tags: ['cool', 'teacher'],
+      address: "Sidney No. 1 Lake Park",
+      tags: ["cool", "teacher"],
     },
   ];
-    return (
-        <div>
-            <Table columns={columns} dataSource={users} />,
-           <Button onClick={()=>{
-             getUsers()
-           }}>Users </Button>
-        </div>
-    )
+  return (
+    <div>
+      <Table columns={columns} dataSource={users} />,
+    
+    </div>
+  );
 }
 
-export default Beneficiary
+export default Beneficiary;
