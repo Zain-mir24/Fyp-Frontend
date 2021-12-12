c94f44819e5e8ea1f49d331e0523401fe5ac700e
Zain-mir24 <zainnaeemk10@gmail.com>
2021-12-12 14:09:36 +0500

    campaign managment done

--- src/components/AdminPanel/adminCampaign.js
+++ src/components/AdminPanel/adminCampaign.js
@@ -1,9 +1,10 @@
 import { TextField } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
 import React, { useEffect, useState } from "react";
-import { Table, Button } from "antd";
+import { Table, Button, Input, Upload, Col, Form} from "antd";
+import { UploadOutlined } from "@ant-design/icons";
 const axios = require("axios");
-function Form() {
+function Foorm() {
   const [camp, setCamp] = useState([]);
   const [name, setName] = useState("");
   const [description, setdesc] = useState("");
@@ -16,7 +17,6 @@ function Form() {
   };
   useEffect(() => {
     viewCamp();
-    
   }, []);
   const postData = async () => {
     const formData = new FormData();
@@ -25,15 +25,13 @@ function Form() {
     formData.append("file", file);
     formData.append("fileName", fileName);
 
-    
     try {
-      
       const res = await axios.post(
         "http://localhost:9000/admin/addCampaign",
         formData
       );
-      if(!res){
-        return console.log("couldnt add")
+      if (!res) {
+        return console.log("couldnt add");
       }
       alert(`Campaign has been added`);
       console.log(res);
@@ -46,33 +44,40 @@ function Form() {
       const res = await axios.get("http://localhost:9000/admin/viewCampaigns");
       console.log(res, "Response from the backend");
       setCamp(
-        res.data.map(i=>({
-             id:i._id,
-             name:i.name
+        res.data.map((i) => ({
+          _id: i._id,
+          name: i.name,
+          description:i.description
         }))
-      )
+      );
     } catch (e) {
-      console.log(e)
+      console.log(e);
     }
   };
 
-  const deleteCamp = async () => {
+  const deleteCamp = async (id) => {
     try {
       const res = await axios.delete(
-        "http://localhost:9000/admin/viewCampaigns"
+        "http://localhost:9000/admin/Deletecampaign/" + id
       );
       console.log(res, "Response from the backend");
-    } catch (e) {}
+      if (!res) {
+        throw new Error("The campaign doesnt exist");
+      }
+      alert(`Campaign has been deleted`);
+    } catch (e) {
+      console.log(e);
+    }
   };
-  
+
   const columns = [
     {
       title: "_id",
-      dataIndex: "id",
-      key: "id",
+      dataIndex: "_id",
+      key: "_id",
       render: (text) => <a>{text}</a>,
     },
-    
+
     {
       title: "Title",
       dataIndex: "name",
@@ -82,57 +87,103 @@ function Form() {
       title: "Action",
       key: "action",
       render: (text, record) => (
-        <button      >
+        <button
+          onClick={() => {
+            deleteCamp(record._id);
+          }}
+        >
           Delete
         </button>
       ),
-    },]
+    },
+  ];
+  
+  const column2 = [
+    {
+      title: "_id",
+      dataIndex: "_id",
+      key: "_id",
+      render: (text) => <a>{text}</a>,
+    },
+
+    {
+      title: "Title",
+      dataIndex: "name",
+      key: "name",
+    },
+    {
+      title: "description",
+      dataIndex: "description",
+      key: "description",
+      
+    },
+  ];
 
   return (
     <div className="row">
+      <h1> Campaign managment </ h1>
+    <div className="col-lg-6">
+        
+        <Form      
+         ><p>
+         Add campaign
+         </p>
+          <Form.Item  rules={[{ required: true, message: "Please Enter campaign name" }]}>
+            <Col span={10}>
+              <Input
+                required
+                placeholder="Campaign name"
+                onChange={(e) => {
+                  setName(e.target.value);
+                }}
+              />
+            </Col>
+          </Form.Item>
+          <Col span={10}>
+          <Form.Item
+          rules={[{ required: true, message: "Please Enter campaign Description" }]}>
+            <Input.TextArea
+              required
+              placeholder="Campaign Description"
+              onChange={(e) => {
+                setdesc(e.target.value);
+              }}
+            />
+          </Form.Item>
+          </Col>
+          <Col span={10}>
+          <Form.Item
+          rules={[{ required: true, message: "Please uplaod doc" }]}
+          onChange={saveFile}
+          >
+            <Upload>
+              <Button icon={<UploadOutlined />}>Upload media files</Button>
+            </Upload>
+          </Form.Item>
+          </Col>
+          <Col span={10}>
+          <Form.Item>
+          <Button type="primary" htmlType="submit"  onClick={postData}>submit</Button>
+          </Form.Item>
+          </Col>
+        </Form>
+        </div>
       <div className="col-lg-6">
-        <form>
-          <TextField
-            variant="outlined"
-            margin="normal"
-            required
-            fullWidth
-            label="Campaign header"
-            name="Campaign header"
-            onChange={(e) => {
-              setName(e.target.value);
-            }}
-          />
-
-          <TextField
-            variant="outlined"
-            margin="normal"
-            required
-            fullWidth
-            label="description"
-            name="description"
-            onChange={(e) => {
-              setdesc(e.target.value);
-            }}
-          />
-          <TextField
-            type="file"
-            variant="outlined"
-            margin="normal"
-            required
-            fullWidth
-            onChange={saveFile}
-          />
-          <Button onClick={postData}>submit</Button>
-        </form>
+        <Table
+          title={() => " Delete campaigns"}
+          columns={columns}
+          dataSource={camp}
+        />
       </div>
-      <div className="col-lg-6">
-        <Table title={() => " Delete campaigns"} columns={columns}
-        dataSource={camp}
+      <div className="col-lg-12">
+        <Table
+          title={() => " All campaigns"}
+          columns={column2}
+          dataSource={camp}
         />
       </div>
     </div>
   );
 }
 
-export default Form;
+export default Foorm;
