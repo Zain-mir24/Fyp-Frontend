2c97bed84e774ff6bb7ada7bc1becb46d31d6145
Zain-mir24 <zainnaeemk10@gmail.com>
2022-02-10 20:12:45 +0500

    global reach campagin page

--- src/components/Frontend/Campaignrender.jsx
+++ src/components/Frontend/Campaignrender.jsx
@@ -2,18 +2,20 @@ import React from "react";
 import "./campaign.css";
 import { Button } from "antd";
 function Campaignrender(props) {
-    console.log(props,"my props")
+  console.log(props, "my props");
   return (
     <div>
       <article>
         <img
-          style={{ width: "100%", borderRadius: "5px" }}
-          src={"https://damp-stream-39096.herokuapp.com/uploads/"+props.img}
-
+          style={{ width: "100%", height: "600px", borderRadius: "5px" }}
+          src={"https://damp-stream-39096.herokuapp.com/uploads/" + props.img}
         ></img>
         <div className="camp">
           <header className="campHead">
             FLOWERS IN YOUR GARDEN. THE BEST SORTS OF FLOWERS FOR OUTDOORS.
+            <div>
+              <span style={{color:"#a1a2a4"}}>By Global reach</span>
+            </div>
           </header>
           <div className="campDesc">
             From art market mainstays to persistent provocateu. It was a tall
@@ -32,7 +34,6 @@ function Campaignrender(props) {
           </div>
         </div>
       </article>
-      
     </div>
   );
 }
