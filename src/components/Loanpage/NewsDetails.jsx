587d19ef03265d5217a9bf83e2295eb9cb320789
Zain-mir24 <zainnaeemk10@gmail.com>
2022-02-13 11:36:23 +0500

    newsDetails

--- src/components/Loanpage/NewsDetails.jsx
+++ src/components/Loanpage/NewsDetails.jsx
@@ -1,11 +1,33 @@
-import React from 'react'
+import React from "react";
 import Footer from "../Footer/Footer";
 import Header from "../Headers/Header";
 import "./Latestnews.css";
 function NewsDetails() {
   return (
-    <div>NewsDetails</div>
-  )
+    <div>
+      <Header />
+     
+      <div style={{marginTop:"-20px"}}>
+        <img className="mainNewsDiv" src={"./Images/NewsDetail.jpg"}></img>
+      </div>
+      <div className="textoverimage">
+        <h1 className="title">News Details for our page</h1>
+        <p>
+          <a style={{ color: "#FFFFFF" }} href="/">
+            Home
+          </a>{" "}
+          /{" "}
+          <a style={{ color: "#FFFFFF" }} href="/News">
+            News
+          </a>{" "} /{" "}
+          <a style={{ color: "#FFFFFF" }} href="/News">
+            NewsDetail
+          </a>{" "}
+        </p>
+      </div>
+      <Footer />
+    </div>
+  );
 }
 
-export default NewsDetails
+export default NewsDetails;
