028c8264b8fc81894c144fa7e775ff0052f72193
Zain-mir24 <zainnaeemk10@gmail.com>
2022-05-13 15:20:18 +0500

    my previous build

--- src/components/Headers/Header.js
+++ src/components/Headers/Header.js
@@ -19,20 +19,20 @@ import globalicon from "../../Images/globalicon.png";
 function Headers() {
   return (
     <nav
-      class="navbar navbar-expand-lg navbar-dark transparent-head fixed-top"
+      className="navbar navbar-expand-lg navbar-dark transparent-head fixed-top"
       style={{
         margin: "0 0 150px",
       }}
     >
-      <a class="navbar-brand" href="">
+      <a className="navbar-brand" href="">
         <img
-          className="navbar-brand"
+          classNameName="navbar-brand"
           src={globalicon}
-          // style={{ position: "absolute", top: "0" }}
+        // style={{ position: "absolute", top: "0" }}
         />
       </a>
       <button
-        class="navbar-toggler"
+        className="navbar-toggler"
         type="button"
         data-toggle="collapse"
         data-target="#navbarSupportedContent"
@@ -40,28 +40,28 @@ function Headers() {
         aria-expanded="false"
         aria-label="Toggle navigation"
       >
-        <span class="navbar-toggler-icon"></span>
+        <span className="navbar-toggler-icon"></span>
       </button>
-      <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav ml-auto">
-          <li class="nav-item ">
-            <a class="nav-link" href="/">
+      <div className="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul className="navbar-nav ml-auto">
+          <li className="nav-item ">
+            <a className="nav-link" href="/">
               Home
             </a>
           </li>
-          <li class="nav-item active">
-            <a class="nav-link" href="/news">
+          <li className="nav-item active">
+            <a className="nav-link" href="/news">
               News
             </a>
           </li>
-          <li class="nav-item">
-            <a class="nav-link" href="/Campaign">
+          <li className="nav-item">
+            <a className="nav-link" href="/Campaign">
               Campaign
             </a>
           </li>
 
-          <li class="nav-item">
-            <a class="nav-link" href="/Video">
+          <li className="nav-item">
+            <a className="nav-link" href="/Video">
               Social Media
             </a>
           </li>
@@ -70,19 +70,19 @@ function Headers() {
       <div style={{ paddingLeft: "150px" }}></div>
     </nav>
     // <nav
-    //   className="navbar navbar-expand-lg"
+    //   classNameName="navbar navbar-expand-lg"
     //   style={{ height: "80px", backgroundColor: "rgba(0, 0, 0, 0.7)" }}
     // >
-    //   <div className="container-fluid">
+    //   <div classNameName="container-fluid">
     //     <div>
     //       <img
-    //         className="navbar-brand"
+    //         classNameName="navbar-brand"
     //         src={globalicon}
     //         style={{ height: "20%" }}
     //       />
 
     //       <button
-    //         className="navbar-toggler"
+    //         classNameName="navbar-toggler"
     //         type="button"
     //         data-bs-toggle="collapse"
     //         data-bs-target="#navbarSupportedContent"
@@ -90,24 +90,24 @@ function Headers() {
     //         aria-expanded="false"
     //         aria-label="Toggle navigation"
     //       >
-    //         <span className="navbar-toggler-icon"></span>
+    //         <span classNameName="navbar-toggler-icon"></span>
     //       </button>
     //     </div>
-    //     <div className="collapse navbar-collapse" id="navbarSupportedContent">
-    //       <ul className="navbar-nav me-auto">
-    //         <li className="nav-item">
-    //           <a className="nav-link active" aria-current="page" href="#">
+    //     <div classNameName="collapse navbar-collapse" id="navbarSupportedContent">
+    //       <ul classNameName="navbar-nav me-auto">
+    //         <li classNameName="nav-item">
+    //           <a classNameName="nav-link active" aria-current="page" href="#">
     //             Home
     //           </a>
     //         </li>
-    //         <li className="nav-item">
-    //           <a className="nav-link" href="#">
+    //         <li classNameName="nav-item">
+    //           <a classNameName="nav-link" href="#">
     //             Link
     //           </a>
     //         </li>
-    //         <li className="nav-item dropdown">
+    //         <li classNameName="nav-item dropdown">
     //           <a
-    //             className="nav-link dropdown-toggle"
+    //             classNameName="nav-link dropdown-toggle"
     //             href="#"
     //             id="navbarDropdown"
     //             role="button"
@@ -117,24 +117,24 @@ function Headers() {
     //             Dropdown
     //           </a>
     //           <ul
-    //             className="dropdown-menu dropdown-menu-end"
+    //             classNameName="dropdown-menu dropdown-menu-end"
     //             aria-labelledby="navbarDropdown"
     //           >
     //             <li>
-    //               <a className="dropdown-item" href="#">
+    //               <a classNameName="dropdown-item" href="#">
     //                 Action
     //               </a>
     //             </li>
     //             <li>
-    //               <a className="dropdown-item" href="#">
+    //               <a classNameName="dropdown-item" href="#">
     //                 Another action
     //               </a>
     //             </li>
     //             <li>
-    //               <hr className="dropdown-divider" />
+    //               <hr classNameName="dropdown-divider" />
     //             </li>
     //             <li>
-    //               <a className="dropdown-item" href="#">
+    //               <a classNameName="dropdown-item" href="#">
     //                 Something else here
     //               </a>
     //             </li>
@@ -147,19 +147,19 @@ function Headers() {
   );
 }
 
-// <nav className="navbar navbar-expand-sm navbar-light bg-success">
-//   <div className="container-fluid ">
-//     <a className="navbar-brand " href="#">Navbar</a>
-//     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-//       <span className="navbar-toggler-icon"></span>
+// <nav classNameName="navbar navbar-expand-sm navbar-light bg-success">
+//   <div classNameName="container-fluid ">
+//     <a classNameName="navbar-brand " href="#">Navbar</a>
+//     <button classNameName="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+//       <span classNameName="navbar-toggler-icon"></span>
 //     </button>
-//     <div className="collapse navbar-collapse " id="navbarSupportedContent">
-//       <ul className="navbar-nav ms-auto">
-//         <li className="nav-item">
-//           <a className="nav-link active" aria-current="page" href="#">Home</a>
+//     <div classNameName="collapse navbar-collapse " id="navbarSupportedContent">
+//       <ul classNameName="navbar-nav ms-auto">
+//         <li classNameName="nav-item">
+//           <a classNameName="nav-link active" aria-current="page" href="#">Home</a>
 //         </li>
-//         <li className="nav-item">
-//           <a className="nav-link" href="#">Course</a>
+//         <li classNameName="nav-item">
+//           <a classNameName="nav-link" href="#">Course</a>
 //         </li>
 //       </ul>
 //     </div>
@@ -170,30 +170,30 @@ export default Headers;
 
 /* 
       <div>
-        <nav className="navbar  navbar-expand-md " style={{ borderWidth: 1, borderColor: "black" }}>
-          <div className="container-fluid">
-            <a className="navbar-brand" href="/">
+        <nav classNameName="navbar  navbar-expand-md " style={{ borderWidth: 1, borderColor: "black" }}>
+          <div classNameName="container-fluid">
+            <a classNameName="navbar-brand" href="/">
               GLOBAL REACH
             </a>
             <button
               data-toggle="collapse"
-              className="navbar-toggler"
+              classNameName="navbar-toggler"
               data-target="#navcol-1"
             >
-              <span className="sr-only">Toggle navigation</span>
-              <span className="navbar-toggler-icon"></span>
+              <span classNameName="sr-only">Toggle navigation</span>
+              <span classNameName="navbar-toggler-icon"></span>
             </button>
-            <div className="collapse navbar-collapse" id="navcol-1">
-              <ul className="nav navbar-nav navbar-right">
-                <li className="nav-item" role="presentation">
-                  <a className="nav-link" href="/">
+            <div classNameName="collapse navbar-collapse" id="navcol-1">
+              <ul classNameName="nav navbar-nav navbar-right">
+                <li classNameName="nav-item" role="presentation">
+                  <a classNameName="nav-link" href="/">
                     Homepage
                   </a>
                 </li>
-                <li className="nav-item dropdown" role="presentation">
-                  <div className="dropdown show" style={{ marginTop: "8px" }}>
+                <li classNameName="nav-item dropdown" role="presentation">
+                  <div classNameName="dropdown show" style={{ marginTop: "8px" }}>
                     <a
-                      className="btn dropdown-toggle"
+                      classNameName="btn dropdown-toggle"
                       href="/News"
                       role="button"
                       id="dropdownMenuLink"
@@ -206,53 +206,53 @@ export default Headers;
                     </a>
 
                     <div
-                      className="dropdown-menu"
+                      classNameName="dropdown-menu"
                       aria-labelledby="dropdownMenuLink"
                     >
-                      <a className="dropdown-item" href="/News"
+                      <a classNameName="dropdown-item" href="/News"
                       >
                         Our  News
                       </a>
-                      <a className="dropdown-item" href="#">
+                      <a classNameName="dropdown-item" href="#">
                         Audit
                       </a>
-                      <a className="dropdown-item" href="#">
+                      <a classNameName="dropdown-item" href="#">
                         Zakat
                       </a>
                     </div>
                   </div>
                 </li> */
 
-/* <li className="nav-item" role="presentation">
-                  <a className="nav-link" href="/News">
+/* <li classNameName="nav-item" role="presentation">
+                  <a classNameName="nav-link" href="/News">
                     News
                   </a>
                 </li> */
 
-/* <li className="nav-item" role="presentation">
-                  <a className="nav-link" href="/campaign">
+/* <li classNameName="nav-item" role="presentation">
+                  <a classNameName="nav-link" href="/campaign">
                     About us
                   </a>
                 </li> */
 
-/* <li className="nav-item" role="presentation">
-                  <a className="nav-link" href="/Campaign"> */
+/* <li classNameName="nav-item" role="presentation">
+                  <a classNameName="nav-link" href="/Campaign"> */
 
-/* <span className="glyphicon glyphicon-user"></span> */
+/* <span classNameName="glyphicon glyphicon-user"></span> */
 
 /* About
                   </a>
                 </li>
 
-                <li className="nav-item" role="presentation">
-                  <a className="nav-link" href="/Campaign">
-                    <span className="glyphicon glyphicon-user"></span> Services
+                <li classNameName="nav-item" role="presentation">
+                  <a classNameName="nav-link" href="/Campaign">
+                    <span classNameName="glyphicon glyphicon-user"></span> Services
                   </a>
                 </li>
 
-                <li className="nav-item" role="presentation">
-                  <a className="nav-link" href="/Signin">
-                    <span className="glyphicon glyphicon-log-in"></span> My Account
+                <li classNameName="nav-item" role="presentation">
+                  <a classNameName="nav-link" href="/Signin">
+                    <span classNameName="glyphicon glyphicon-log-in"></span> My Account
                   </a>
                 </li>
               </ul> */
