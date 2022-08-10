8d5f0c6b8f668a1087a2d91c65241a3b417803b3
AHMED KHALID <fa18-bcs-007@isbstudent.comsats.edu.pk>
2022-08-10 14:07:24 +0500

    Updated Heroku API link

--- src/config/axios.js
+++ src/config/axios.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const customInstance = axios.create({
-  baseURL: "http://localhost:9000/",
-  // baseURL: "https://graphql-cannaby-consumer.herokuapp.com/",
+  // baseURL: "http://localhost:9000/",
+  baseURL: "https://pacific-refuge-71507.herokuapp.com/",
   // baseURL: "http://localhost:4000/",
   headers: { Accept: "application/json" },
 });
